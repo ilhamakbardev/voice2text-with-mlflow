53417dd64a15a692b651509aaf9329831f39deae
m.ilham.akbar_ <vicase312@gmail.com>
2025-05-09 07:44:34 +0700

    fix: debug device

--- utils/transcriber.py
+++ utils/transcriber.py
@@ -3,6 +3,7 @@ import time
 import mlflow
 from utils.ffmpeg_path import get_ffmpeg_path
 import os
+import streamlit as st
 
 import torch
 
@@ -16,6 +17,8 @@ def transcribe_audio(audio_path: str, model_name: str):
         ffmpeg_path) + os.pathsep + os.environ["PATH"]
 
     model = whisper.load_model(model_name)
+
+    st.info(f"Using device: {device.upper()}")
 
     start = time.time()
     result = model.transcribe(audio_path, language="indonesian")
